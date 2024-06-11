dd68fd8db5beb3706d75779cb729bf62629e84b6
Aniket Chowdhury <aniket.yuvlucr92@gmail.com>
2024-06-11 15:18:53 +0530

    first commit

---
+++ public/js/home.js
@@ -0,0 +1,11 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.body'),
+    smooth: true
+});
+
+
+VanillaTilt.init(document.querySelectorAll("#tilt"), {
+    max: 25,
+    speed: 400
+});
+
