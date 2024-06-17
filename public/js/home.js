8e3543cbec04a9fc475c3a500422eced54438ac1
Aniket Chowdhury <aniket.yuvlucr92@gmail.com>
2024-06-17 19:16:25 +0530

    second time

--- public/js/home.js
+++ public/js/home.js
@@ -3,9 +3,37 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navItems = document.getElementById('nav-items1');
 
-VanillaTilt.init(document.querySelectorAll("#tilt"), {
-    max: 25,
-    speed: 400
+    hamburger.addEventListener('click', () => {
+        if (navItems.style.display === 'flex') {
+            navItems.style.display = 'none';
+        } else {
+            navItems.style.display = 'flex';
+        }
+    });
 });
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const videos = document.querySelectorAll('#video');
+
+    videos.forEach(video => {
+        video.addEventListener('mouseenter', () => {
+            video.play();
+        });
+
+        video.addEventListener('mouseleave', () => {
+            video.pause();
+            video.currentTime = 0; // Optional: reset the video to the beginning
+        });
+    });
+});
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
