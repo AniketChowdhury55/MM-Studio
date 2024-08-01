a9730c43e62f9425332b96815826b6cbfeb00265
Aniket Chowdhury <aniket.yuvlucr92@gmail.com>
2024-08-01 10:17:04 +0530

    version5.02 >ENV UPDATE

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-if(process.env.WEB_FORMS != "production") {
-    require('dotenv').config();
-  }
   const { render } = require("ejs");
+  const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const express = require("express");
 const app = express();
 const path = require("path");
+require('dotenv').config();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route to handle form submission
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body
+  
+    // Set up the email transporter
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USER_FROM_EMAIL, // Your Gmail address
+        pass: process.env.NODE_MAILER , // Your Gmail password (or app password)
+      },
+    });
+
+   // Email options
+   let mailOptions = {
+    from: email,
+    to: process.env.USER_TO_EMAIL, // Your Gmail address
+    subject: 'New message from website contact form',
+    text: `You have a new message from ${name} (${email}):\n\n${message}`,
+  };   
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send('Error sending email');
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send('Email sent successfully');
+        }
+      });
+    });
 
 app.get("/", (req, res) => {
     res.send("hi, i m root")
